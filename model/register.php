63bce573fa55ff2b4d909349ca89fd0917212cfc
Sonu Yadav <sonukyadav2510@gmail.com>
2021-06-06 12:51:39 +0530

    Update register.php

--- model/register.php
+++ model/register.php
@@ -1,7 +1,7 @@
 <!---->
 
 <?php 
-  include("connectdb.php");
+  
   require("../healper.php");
 session_start();
 if(isset($_POST['submit'])){
@@ -12,7 +12,7 @@ if(isset($_POST['submit'])){
    $pwd     =    rand(1000,9999);
    
      
-   $query = "INSERT INTO `user`(`user_id`, `name`, `pwd`) VALUES ('$user_id','$name','$pwd')";
+   
    $fire = true;
   if($fire){
        $_SESSION['id'] = $user_id;
